3fdbf75bf075a85db83e051ad15cca806065cd1e
Lucas Eiji <104649020+eijilucas@users.noreply.github.com>
2023-05-19 02:37:07 -0300

    order-item

--- src/main/java/com/wepapp/workshop/config/TestConfig.java
+++ src/main/java/com/wepapp/workshop/config/TestConfig.java
@@ -56,10 +56,10 @@ public class TestConfig implements CommandLineRunner{
 		
 		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
-		p2.getCategories().add(cat3);
 		p3.getCategories().add(cat3);
 		p4.getCategories().add(cat3);
 		p5.getCategories().add(cat2);
+		p2.getCategories().add(cat3);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
