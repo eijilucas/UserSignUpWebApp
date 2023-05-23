9837b235f84368d351ffd4a3462f4ebb4e3959fc
Lucas Eiji <104649020+eijilucas@users.noreply.github.com>
2023-05-23 19:45:36 -0300

    exception-handling

--- src/main/java/com/wepapp/workshop/services/UserServices.java
+++ src/main/java/com/wepapp/workshop/services/UserServices.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.wepapp.workshop.entity.User;
 import com.wepapp.workshop.repository.UserRepository;
+import com.wepapp.workshop.services.exceptions.DatabaseException;
+import com.wepapp.workshop.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserServices {
@@ -20,7 +26,7 @@ public class UserServices {
 	
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert (User obj) {
@@ -28,13 +34,23 @@ public class UserServices {
 	}
 	
 	public void delete (Long id) {
+		try {
 		userRepository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update (Long id, User obj) {
+		try {
 		User entity = userRepository.getReferenceById(id);
 		updateData(entity, obj);
 		return userRepository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
---
+++ src/main/java/com/wepapp/workshop/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.wepapp.workshop.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+
+}
